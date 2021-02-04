720583527a13b6ba08fe5fe3173f946acd5e1ed7
Jonathan Andrade <jonathan.andrade@finantech.com.ec>
2021-02-04 11:29:33 -0500

    Validate if a specific time is in or out of a range.

---
+++ Jest/Class/validateTimeRange.js
@@ -0,0 +1,11 @@
+//validate if time is in or out of a range.
+var range = ['07:00','09:00','16:00','19:30']
+function validateTimeRange(time){
+    if(time<range[0] || (time > range[1]&&time < range[2]) || time > range[3]){
+        return (true);
+    }else{
+        return(false);
+    }
+}
+
+module.exports = validateTimeRange;
---
+++ Jest/Test/validateTimeRange.test.js
@@ -0,0 +1,17 @@
+const validateTimeRange = require('../Class/validateTimeRange');
+
+test('valide 23:59 to true', ()=>{
+    expect(validateTimeRange("23:59")).toBe(true);
+});
+
+test('valide 07:59 to false', ()=>{
+    expect(validateTimeRange("07:59")).toBe(false);
+});
+
+test('valide 09:31 to true', ()=>{
+    expect(validateTimeRange("09:01")).toBe(true);
+});
+
+test('valide 09:30 to false', ()=>{
+    expect(validateTimeRange("09:00")).toBe(false);
+});
